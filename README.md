3e8de2f6670b54f025e830adfaf40542c9f4dc80
Fladir <fladireis@gmail.com>
2017-11-01 22:40:47 -0200

    Update README.md

--- README.md
+++ README.md
@@ -2,10 +2,23 @@
 
 # Itelios Frontend Challenge
 
-Olá, meu Nome é Flávio Diego dos Reis (Fladir), e este é o resultado do meu desafio.
+Segue resultado do desafio proposto. [Clique Aqui](https://github.com/iteliosbrasil/itelios-frontend-challenge) para ver as instruções.
 
 ## Como Executar
 
-Acesse o link https://fladireis.github.io/itelios-frontend-challenge/
+Acesse o [Este Link](https://fladireis.github.io/itelios-frontend-challenge/) para conferir o resultado o qual está hospedado dentro do GitHub Pages.
 
-Em breve mais detalhes.
+## Detalhes do Projeto
+
+O código que consome o aquivo .json foi desenvolvido com javascript puro.
+Os dados obtidos são integrados a uma vitrine de produtos que tem sua estrutura 
+completamente responsiva. 
+
+
+Utilizei a biblioteca do [Tiny Slider](https://github.com/ganlanyuan/tiny-slider) 
+para auxiliar na construção do carrossel de produtos. Ele "gira" automaticamente. Quando 
+posicionado o cursor do mouse ou quando acionado o touch em cima de algum produto deste 
+carrossel ele se mantém pausado. 
+
+Usei o Sass como pré-processador para estruturar o CSS, procurei manter uma boa organização e
+como dito anteriormente, tem a estrutura responsiva. 
--- js/scripts.js
+++ js/scripts.js
@@ -9,10 +9,10 @@ var getProductsData = new XMLHttpRequest();
 
 getProductsData.onreadystatechange = function() {
 
-    //checando a resposta do servidor
+    //checking for when response is received from server
     if (getProductsData.readyState === 4) {
 
-        //limitar o tamanho do nome do produto
+        //trim name of product
         function truncate(string, limit) {
             if (string.length > limit) {
                 limit--;
@@ -33,7 +33,7 @@ getProductsData.onreadystatechange = function() {
             return string;
         }
 
-        //obter dados dos produtos visitados
+        //get data of visited product
         var productInfo = JSON.parse(getProductsData.responseText)[0];
         var visitedProduct = productInfo.data.item,
             visitedProductId = visitedProduct.businessId,
@@ -43,7 +43,7 @@ getProductsData.onreadystatechange = function() {
             visitedProductOldPrice = visitedProduct.oldPrice,
             visitedProductInfo = visitedProduct.productInfo.paymentConditions;
 
-        //criar tags das informações dos produtos visitados
+        //create tags of visited product informations
         var visitedImage = '<img src="' + visitedProductImage + '" />';
         var visitedName = '<p class="product-name">' + truncate(visitedProductName, 80) + '</p>';
 
@@ -60,13 +60,13 @@ getProductsData.onreadystatechange = function() {
             visitedproductInfoHtml = '<p class="product-pay-conditions">' + newProductInfo + '</p>';
         var visitedButton = '<a href="' + document.location.origin + '/itelios-challenge/' + visitedProductId + '" class="product-link"><span>adicionar ao carrinho</span><i class="material-icons">add_shopping_cart</i></a>';
 
-        visitedProductHtml = '<a class="product-anchor" href="#">' + visitedImage + visitedName + visitedPrice + visitedproductInfoHtml + visitedButton + '</a>';
+        visitedProductHtml = '<div>' + visitedImage + visitedName + visitedPrice + visitedproductInfoHtml + visitedButton + '</div>';
 
-        //inserir produtos na área de produtos visitados
+        //insert product in visited area
         document.getElementById("visited").innerHTML = visitedProductHtml;
 
 
-        //obter dados dos produtos recomendados
+        //get data of recommended products
         var recommendations = productInfo.data.recommendation;
         var recomendationsProductHtml = '';
         for (var i = 0; i < recommendations.length; i++) {
@@ -77,7 +77,7 @@ getProductsData.onreadystatechange = function() {
                 recommendedProductOldPrice = recommendations[i].oldPrice,
                 recommendedProductInfo = recommendations[i].productInfo.paymentConditions;
 
-            //criar tags das informações dos produtos recomendados
+        //create tags of recommended product informations
             var recommendedImage = '<img src="' + recommendedProductImage + '" />';
             var recommendedName = '<p class="product-name">' + truncate(recommendedProductName, 80) + '</p>';
 
@@ -94,35 +94,44 @@ getProductsData.onreadystatechange = function() {
                 recommendedInfoHtml = '<p class="product-pay-conditions">' + newProductInfo + '</p>';
             var recommendedButton = '<a href="' + document.location.origin + '/itelios-challenge/' + recommendedProductId + '" class="product-link"><span>adicionar ao carrinho</span><i class="material-icons">add_shopping_cart</i></a>';
 
-            recomendationsProductHtml += '<div class="product-item"><a class="product-anchor" href="#">' + recommendedImage + recommendedName + recommendedPrice + recommendedInfoHtml + recommendedButton + '</a></div>';
+            recomendationsProductHtml += '<div class="product-item">' + recommendedImage + recommendedName + recommendedPrice + recommendedInfoHtml + recommendedButton + '</div>';
         }
-        //inserir produtos na área de produtos recomendados
+        //insert products in recommended carousel
         document.getElementById("recommended").innerHTML = recomendationsProductHtml;
 
-        //configurações do carrossel
+        //Tiny Slider settings
         var slider = tns({
             container: '.recommended-carousel',
             autoplay: true,
             controls: false,
             autoplayButtonOutput: false,
-            speed: 700,
+            speed: 1200,
             autoplayHoverPause: true,
+			slideBy: 'page',
             responsive: {
-                980: {
-                    items: 3
-                },
-                760: {
-                    items: 2
-                },
-                480: {
-                    items: 1
-                }
-            },
-            slideBy: 'page'
+             980: {
+					items: 3
+				},
+             760: {
+					items: 2
+				},
+				480: {
+					items: 1
+				}
+            }
+            
 
         });
+
+		//wrap carousel nav pagination
+		var el = document.querySelector('.tns-nav');
+		var wrapper = document.createElement('div');
+		wrapper.className = "tns-nav-wrapper";
+		el.parentNode.insertBefore(wrapper, el);
+		wrapper.appendChild(el);
     }
 };
 
 getProductsData.open("GET", "products.json", true);
 getProductsData.send();
+
